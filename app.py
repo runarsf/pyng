173b1730304f11822bdb18a2575778770edfef4b
Runar Fredagsvik <runarsf@protonmail.com>
2020-04-15 20:01:00 +0100

    ding ding ding

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import subprocess
 import random
 import string
 import os
+import sys
 import datetime
 import pytz
+import logging
 from flask import Flask, request, render_template, redirect, jsonify
 
 app = Flask(__name__)
@@ -13,60 +15,68 @@ app = Flask(__name__)
 
 # amixer  sset PCM,0 95%
 
-textLimit = 200
-nameLimit = 50
-apiKey = os.getenv('API_KEY', '5u93r53cr37491k3y')
+text_limit = 200
+name_limit = 50
+api_key = os.getenv('API_KEY', '5u93r53cr37491k3y')
+port = os.getenv('PORT', 8009)
 
-# The actual decorator function
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.DEBUG,
+    format='[%(asctime)s] [%(levelname)s] %(message)s', # %(name)s
+    datefmt='%d/%m/%Y %H:%M:%S'
+)
+log = logging.getLogger(__name__)
+
 def require_appkey(view_function):
-  @wraps(view_function)
-  # the new, post-decoration function. Note *args and **kwargs here.
-  def decorated_function(*args, **kwargs):
-    if 'api_key' in request.args and str(request.args['api_key']) == str(apiKey):
-      return view_function(*args, **kwargs)
-    else:
-      abort(401)
-    return decorated_function
+    @wraps(view_function)
+    def decorated_function(*args, **kwargs):
+        if 'api_key' in request.args and str(request.args['api_key']) == str(api_key):
+            return view_function(*args, **kwargs)
+        else:
+            abort(401)
+        return decorated_function
 
 @app.route('/')
-def form():
-  return render_template('form.html')
+def index():
+    return render_template('form.html')
 
 @app.route('/post', methods=['GET', 'POST'])
 def poster():
-  text = request.args.get('text').replace('\\', '').replace('\'', '').replace('\"', '')
-  name = request.args.get('name')
-  #text = request.form['text'].replace('\\', '').replace('\'', '').replace('\"', '')
-  if not text:
-    return redirect(request.url_root)
-  #name = request.form['name']
-  print(f'{request.remote_addr}|{name}> {text}')
-  logger(text, name, request.remote_addr)
-  illegal = []
-  if any(bad in text for bad in illegal):
-    return f'<h1 style="font-family: sans-serif;">Wait that\'s illegal! ({", ".join(illegal)})</h1>'
-  if len(text) > textLimit or len(name) > nameLimit:
-    return f'<h1>Message cannot exceed {limit} characters.</h1>'
-  #subprocess.run(['say', f'{text}{". From " + name if name else ""}'])
-  #subprocess.run(['echo', f'{text}{". From " + name if name else ""}', '|', 'festival', '--tts'])
+    name = request.args.get('name')
+    text = request.args.get('text') \
+        .replace('\\', 'backslash') \
+        .replace('\'', 'quote') \
+        .replace('\"', 'quotes') \
+        .replace('|', 'pipe') \
+        .replace(';', 'semicolon') \
+        .replace('&', 'and') \
+        .replace('<', 'less than') \
+        .replace('>', 'greater than') \
+        .replace('$', 'dollar') \
+        .replace('`', 'tick')
+    #name = request.form['name']
+    #text = request.form['text'].replace('\\', '').replace('\'', '').replace('\"', '')
+    if not text:
+        return redirect(request.url_root)
+    log.info(f'{name}@{request.remote_addr} {text}')
+    illegal = []
+    if any(bad in text for bad in illegal):
+        return f'<h1 style="font-family: sans-serif;">Wait that\'s illegal! ({", ".join(illegal)})</h1>'
+    if len(text) > text_limit or len(name) > name_limit:
+        return f'<h1 style="font-family: sans-serif;">' \
+               f'Message cannot exceed {text_limit} characters.<br/>' \
+               f'Name cannot exceed {name_limit} characters.</h1>'
 
-  ps = subprocess.Popen(('echo', f'{text}{". From " + name if name else ""}'), stdout=subprocess.PIPE)
-  output = subprocess.check_output(('festival', '--tts'), stdin=ps.stdout)
-  ps.wait()
+    ps = subprocess.Popen(('echo', f'{text}{". From " + name if name else ""}'), stdout=subprocess.PIPE)
+    output = subprocess.check_output(('festival', '--tts'), stdin=ps.stdout)
+    ps.wait()
 
-  if request.method == 'GET':
-    return redirect(request.url_root)
-  else:
-    return jsonify('thank you')
-
-def logger(message: str, user: str ='', ip: str = ''):
-  time = str(datetime.datetime.now(pytz.timezone('Europe/Oslo')).strftime('%d-%m-%y %H:%M:%S'))
-  #time = str(datetime.datetime.now().strftime('%d-%m-%y %H:%M:%S'))
-  with open('messages.log', 'a') as logfile:
-    logfile.write(f'[{user}@{ip}] {time} - {message}\n')
-
+    if request.method == 'GET':
+        return redirect(request.url_root)
+    else:
+        return jsonify('thank you for the kind words')
 
 if __name__ == '__main__':
-  app.config['DEBUG'] = False
-  app.run(host='0.0.0.0', port=8009)
-
+  app.config['DEBUG'] = True
+  app.run(host='0.0.0.0', port=port)
